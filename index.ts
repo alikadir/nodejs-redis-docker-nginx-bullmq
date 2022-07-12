659cf092aae6070923b108030d8c4481a087de94
Ali K. Bagcioglu <alikadirbagcioglu@gmail.com>
2022-07-13 01:31:59 +0300

    add eslint (airbnb)

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hi! " + Date());
+app.get('/', (req: Request, res: Response) => {
+  res.send(`Hi! ${Date()}`);
 });
 
 app.listen(port, () => {
